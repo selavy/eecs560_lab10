28400d888a9f3f27d8c2a4ad356b7fa75ec66c29
Peter Lesslie <pclesslie@gmail.com>
2014-04-10 17:11:52 -0500

    Add print function

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -4,6 +4,9 @@ DisjointSet::DisjointSet() : height_( 0 ), width_( 0 ) {
 }
 
 DisjointSet::~DisjointSet() {
+  for( auto it : set_ ) {
+    delete it;
+  }
 }
 
 DisjointSet::DisjointSet( int height, int width ) : height_( height ), width_( width ) {
@@ -12,22 +15,39 @@ DisjointSet::DisjointSet( int height, int width ) : height_( height ), width_( w
   //
   int size = height * width;
   for( int i = 0; i < size; ++i ) {
-    map_[i] = i;
+    Node * node = new Node( i, NULL );
+    set_.push_back( node );
   }
 }
 
 void DisjointSet::print( std::ostream& os ) {
   using namespace std;
-  for( int h = 0; h < height_; ++h ) {
-    for( int w = 0; w < width_; ++w ) {
-      cout << " ---";
+
+  int height = 0;
+  for( int i = 0; i < static_cast<int>( set_.size() ); ++i ) {
+    if( i != 0 ) {
+      if( set_.at( i )->walls[WEST] ) {
+	cout << "   |";
+      } else {
+	cout << "    ";
+      }
     }
-    cout << endl << "|";
-    for( int w = 0; w < width_; ++w ) {
-      cout << "   |";
+
+    if( ( i % width_ ) == 0 ) {
+      cout << endl;
+      for( int w = 0; w < width_; ++w ) {
+	if( set_.at( width_ * height + w )->walls[NORTH] ) {
+	  cout << " ---";
+	} else {
+	  cout << "    ";
+	}
+      }
+      cout << endl << "|";
+      ++height;
     }
-    cout << endl;
   }
+
+  cout << "   |" << endl;
   for( int w = 0; w < width_; ++w ) {
     cout << " ---";
   }
--- DisjointSet.hpp
+++ DisjointSet.hpp
@@ -2,9 +2,24 @@
 #define _DISJOINTSET_
 
 #include <iostream>
-#include <unordered_map>
+#include <vector>
 
 class DisjointSet {
+public:
+  enum {
+    NORTH = 0,
+    WEST,
+  };
+
+  struct Node {
+    int elem;
+    bool walls[2];
+    Node * parent;
+
+    Node( int x, Node* p ) : elem( x ), parent( p ) {
+      for( int i = 0; i < 2; ++i ) walls[i] = true;
+    }
+  };
 public:
   DisjointSet();
   DisjointSet( int height, int width );
@@ -13,7 +28,7 @@ public:
 private:
   int height_;
   int width_;
-  std::unordered_map<int, int> map_;
+  std::vector<Node*> set_;
 };
 
 #endif
